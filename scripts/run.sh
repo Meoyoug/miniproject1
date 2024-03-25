e3eccad4aaf08ee0fb74b45bd29af207087c7b04
Meoyoug <eodyd426@gmail.com>
2024-03-25 17:36:36 +0900

    배포전

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-
-# python wait_for_db.py
-
-gunicron -k uvicorn.workers.UvicornWorker main:app --bind 127.0.0.1:8000
+gunicorn -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000
 
 alembic upgrade head
 
